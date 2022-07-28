8a21ef95f5163eae0c0a45236197b5c8b0380c0b
GeorgeAndzhich <djordjeandjic99@gmail.com>
2022-07-28 15:20:44 +0200

    done

--- HospitalApp/doctor/queries.js
+++ HospitalApp/doctor/queries.js
@@ -2,10 +2,12 @@ const getDoctors = "SELECT * FROM doctors";
 const getDoctorByID = "SELECT * FROM doctors WHERE id = $1";
 const addDoctor = "INSERT INTO doctors(id,name,age,specialization) values($1,$2,$3,$4)";
 const deleteDoctor = "DELETE FROM doctors where id = $1";
+const updateDoctor = "UPDATE doctors SET age = $1 WHERE id = $2";
 
 module.exports = {
     getDoctors,
     getDoctorByID,
     addDoctor,
     deleteDoctor,
+    updateDoctor,
 }
